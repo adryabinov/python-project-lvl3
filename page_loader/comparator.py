7664910994d30e86b4dae5581e04c4e6aaca9cd7
andrejrabinov <andryabinov@gmail.com>
2021-03-04 15:51:27 +0300

    fix filename converter

--- page_loader/comparator.py
+++ page_loader/comparator.py
@@ -1,10 +1,24 @@
 import os
+import re
 
 def download(url: str, path: str) -> str:
+
     abs_path = os.path.abspath(path)
 
     if not os.path.isdir(abs_path):
         raise IOError (f'{abs_path} is not found')
 
-    return str(abs_path)
+    path_to_file = os.path.join(
+        abs_path,
+        url_to_filename(url)
+    )
 
+    return str(path_to_file)
+
+def url_to_filename(url: str) -> str:
+
+    drop_scheme_url = re.sub('.*?://', '', url, 1)
+    replaced_symbols = re.sub('[^a-zA-Z0-9]', '-', drop_scheme_url)
+    filename = f"{replaced_symbols}.html"
+
+    return filename
