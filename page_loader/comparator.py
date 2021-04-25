6b005248e1993680c5e471ba6417dac5851b4b89
andrejrabinov <andryabinov@gmail.com>
2021-04-25 14:45:15 +0300

    add tests and logic

--- page_loader/comparator.py
+++ page_loader/comparator.py
@@ -1,40 +1,77 @@
 import os
-import re
 import requests
+import logging
+import urllib.parse as up
+from bs4 import BeautifulSoup
+
+from page_loader.restools import save_file
+from page_loader.restools import save_res
+from page_loader.restools import mk_name
 
 
-def load_url(url: str) -> str:
+logging.basicConfig(level=logging.INFO)
 
+
+def prepare_res(html, url, folder_path):
+    map_tag_to_attr = {
+        "link": "href",
+        "script": "src",
+        "img": "src"
+    }
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = []
+    for tag in map_tag_to_attr.keys():
+        tags.extend(soup.find_all(tag))
+
+    links = []
+    for tag in tags:
+        attr = map_tag_to_attr[tag.name]
+        link = tag.get(attr)
+
+        if not link:
+            continue
+
+        full_url = up.urljoin(f"{url}/", link)
+
+        if up.urlparse(full_url).netloc != up.urlparse(url).netloc:
+            continue
+
+        file_name = mk_name(full_url)
+        file_path = os.path.join(folder_path, file_name)
+        tag[attr] = file_path
+        links.append({
+            "link": full_url,
+            "file_path": file_path
+        })
+    return links, soup.prettify(formatter="html5")
+
+
+def download(url, output=os.getcwd()):
+    html_name = mk_name(url)
+    html_path = os.path.join(output, html_name)
+    file_folder_name = mk_name(url, is_dir=True)
+    file_folder_path = os.path.join(output, file_folder_name)
+
+    logging.info(f"GET {url}")
     response = requests.get(url)
-    return response.text
+    response.raise_for_status()
+    logging.info("Done.")
 
+    logging.info(f"MAKE RESOURCES DIR AT: '{file_folder_path}'")
+    if not os.path.exists(file_folder_path):
+        os.mkdir(file_folder_path)
+    logging.info("Done.")
 
-def save_content(content, path: str):
-    with open(path, "w+") as f:
-        return f.write(content)
+    logging.info("Preparing resources...")
+    resources, html = prepare_res(response.text, url, file_folder_name)
+    logging.info("Done.")
 
+    logging.info("Downloading resources:")
+    for resource in resources:
+        save_res(resource, output)
+    logging.info("Done.")
 
-def download(url: str, path: str) -> str:
-
-    abs_path = os.path.abspath(path)
-
-    if not os.path.isdir(abs_path):
-        raise IOError(f'{abs_path} is not found')
-
-    path_to_file = os.path.join(
-        abs_path,
-        url_to_filename(url)
-    )
-
-    save_content(load_url(url), path_to_file)
-
-    return str(path_to_file)
-
-
-def url_to_filename(url: str) -> str:
-
-    drop_scheme_url = re.sub('.*?://', '', url, 1)
-    replaced_symbols = re.sub('[^a-zA-Z0-9]', '-', drop_scheme_url)
-    filename = f"{replaced_symbols}.html"
-
-    return filename
+    logging.info(f"Saving html to {html_path}")
+    save_file(html, html_path)
+    logging.info("Done.")
+    return html_path
--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
+import logging
+import sys
+
 from page_loader.argparse import parse_arguments
 from page_loader.comparator import download
 
 
 def main():
-    args = parse_arguments()
-    print(download(args.url, args.output))
+    try:
+        args = parse_arguments()
+        print(download(args.url, args.output))
+    except Exception as error:
+        logging.error(error)
+        sys.exit(1)
 
 
 if __name__ == '__main__':
