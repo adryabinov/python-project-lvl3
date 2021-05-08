add7e7801e7adddc650ca978c15ebf67c293f5fd
andrejrabinov <andryabinov@gmail.com>
2021-05-08 16:05:26 +0300

    ref 4 naming

--- page_loader/page.py
+++ page_loader/page.py
@@ -1,15 +1,15 @@
 import os
 import requests
 import logging
-import page_loader.resourse
+import page_loader.resourсe
 
 logging.basicConfig(level=logging.INFO)
 
 
 def download(url, output=os.getcwd()):
-    html_name = page_loader.resourse.make_name(url)
+    html_name = page_loader.resourсe.make_name(url)
     html_path = os.path.join(output, html_name)
-    file_folder_name = page_loader.resourse.make_name(url, is_dir=True)
+    file_folder_name = page_loader.resourсe.make_name(url, is_dir=True)
     file_folder_path = os.path.join(output, file_folder_name)
 
     logging.info(f"GET {url}")
@@ -23,16 +23,16 @@ def download(url, output=os.getcwd()):
     logging.info("Done.")
 
     logging.info("Preparing resources...")
-    resources, html = page_loader.resourse.prepare(
+    resources, html = page_loader.resourсe.prepare(
         response.text, url, file_folder_name)
     logging.info("Done.")
 
     logging.info("Downloading resources:")
     for resource in resources:
-        page_loader.resourse.download(resource, output)
+        page_loader.resourсe.download(resource, output)
     logging.info("Done.")
 
     logging.info(f"Saving html to {html_path}")
-    page_loader.resourse.save(html, html_path)
+    page_loader.resourсe.save(html, html_path)
     logging.info("Done.")
     return html_path
